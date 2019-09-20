2d1ca3648cb967f0c8c2cf196f5f544ede611bf2
vlafed <vollfed@mail.ru>
2019-09-20 23:52:46 +0300

    lets go

---
+++ src/main/java/test/home/StartKinect.java
@@ -0,0 +1,34 @@
+package test.home;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class StartKinect {
+
+    static final Logger logger = LogManager.getLogger(StartKinect.class.getName());
+
+    public static void main(String[] args) {
+
+        StartKinect obj = new StartKinect();
+        obj.runMe("mkyong");
+
+    }
+
+    private void runMe(String parameter){
+
+        if(logger.isDebugEnabled()){
+            logger.debug("This is debug : " + parameter);
+        }
+
+        if(logger.isInfoEnabled()){
+            logger.info("This is info : " + parameter);
+        }
+
+        logger.info("This is info : " + parameter);
+        logger.debug("This is debug : " + parameter);
+        logger.warn("This is warn : " + parameter);
+        logger.error("This is error : " + logger.isDebugEnabled());
+        logger.fatal("This is fatal : " + parameter);
+        logger.error("This is error  : " + logger.isInfoEnabled());
+    }
+}
